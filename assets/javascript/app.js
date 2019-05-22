44edac10a8e5b0f9325dafa485ab549097b0ac94
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-05-22 11:37:56 -0700

    fix alphabet array and test if letter is randomly picked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var guess;
 
 var letter;
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0; 
 var losses = 0;
@@ -14,8 +14,9 @@ var guesses = [];
 
 document.onkeyup = function(event) {
     guess = event.key;
-    console.log(guess);
     letter = alphabet[Math.floor(alphabet.length * Math.random())];
+    console.log(letter);
+    
     guesses.push(guess);
     document.getElementById("guesses").textContent = document.getElementById("guesses").textContent + " " + guess;
 }
