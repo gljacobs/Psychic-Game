6c21c99a52a3865a6c2dc752c5c0f8c96b6b9c3b
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-05-22 11:58:15 -0700

    update site according to wins, losses, guesses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,9 @@
 //Psychic App - Javascript 
 
 var guess;
-
-var letter;
-
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letter = alphabet[Math.floor(alphabet.length * Math.random())];
+
 
 var wins = 0; 
 var losses = 0;
@@ -14,10 +13,21 @@ var guesses = [];
 
 document.onkeyup = function(event) {
     guess = event.key;
-    letter = alphabet[Math.floor(alphabet.length * Math.random())];
     console.log(letter);
     
-    guesses.push(guess);
-    document.getElementById("guesses").textContent = document.getElementById("guesses").textContent + " " + guess;
+    if(guessesLeft > 0) {
+        guesses.push(guess);
+        document.getElementById("guesses").textContent = document.getElementById("guesses").textContent + " " + guess;
+        if(guess === letter) {
+            wins++;
+            document.getElementById("games-won").textContent = "Wins: " + wins;
+        }
+    }
+    else {
+        losses++;
+        document.getElementById("games-lost").textContent = "Losses: " + losses;
+    }
+    guessesLeft--;
+    document.getElementById("guesses-left").textContent = "Guesses Left: " + guessesLeft;
 }
 
